a2dd97ba595cfe5cf117c06b5c16f485ea3011dc
Raiden <adrien.graindorge@my-digital-school.org>
2023-12-14 23:28:55 +0100

    ADD: nav

--- js/menu.js
+++ js/menu.js
@@ -1,37 +1,23 @@
-// Récupérer les éléments DOM
-var menuToggle = document.getElementById('menu-toggle');
-var navigation = document.querySelector('.nav');
+document.addEventListener("DOMContentLoaded", function () {
+    var menuToggle = document.getElementById('menu-toggle');
+    var navigation = document.querySelector('.nav');
 
-// Ajouter un gestionnaire d'événements au bouton de bascule du menu
-menuToggle.addEventListener('click', function () {
-    // Basculer la visibilité du menu de navigation
-    navigation.style.display = (navigation.style.display === 'none' || navigation.style.display === '') ? 'flex' : 'none';
-});
+    menuToggle.addEventListener('click', function () {
+        navigation.classList.toggle('active');
+    });
 
-// Ajouter un gestionnaire d'événements pour masquer le menu lorsqu'un lien est cliqué (pour les écrans plus petits)
-var navLinks = document.querySelectorAll('.lien');
-navLinks.forEach(function (link) {
-    link.addEventListener('click', function () {
-        if (window.innerWidth <= 1200) {
-            navigation.style.display = 'none';
+    var navLinks = document.querySelectorAll('.lien');
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
+            if (window.innerWidth <= 1200) {
+                navigation.classList.remove('active');
+            }
+        });
+    });
+
+    window.addEventListener('resize', function () {
+        if (window.innerWidth > 1200) {
+            navigation.classList.add('active');
         }
     });
 });
-
-// Ajouter un gestionnaire d'événements pour masquer le menu lorsqu'on redimensionne l'écran
-window.addEventListener('resize', function () {
-    if (window.innerWidth > 1200) {
-        navigation.style.display = 'flex';
-    }
-});
-
-// Récupérer les éléments DOM
-var menuToggle = document.getElementById('menu-toggle');
-var navigation = document.querySelector('.nav');
-
-// Ajouter un gestionnaire d'événements au bouton de bascule du menu
-menuToggle.addEventListener('click', function () {
-    // Basculer la classe active pour afficher ou masquer le menu déroulant
-    navigation.classList.toggle('active');
-});
-
