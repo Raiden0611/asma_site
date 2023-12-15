60d46a3e8fbf90aee97d6a4ccec8476002316390
Raiden <adrien.graindorge@my-digital-school.org>
2023-12-15 10:30:23 +0100

    ADD: NEW ITEM FOR NAV

--- js/menu.js
+++ js/menu.js
@@ -1,23 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var menuToggle = document.getElementById('menu-toggle');
-    var navigation = document.querySelector('.nav');
-
-    menuToggle.addEventListener('click', function () {
-        navigation.classList.toggle('active');
-    });
-
-    var navLinks = document.querySelectorAll('.lien');
-    navLinks.forEach(function (link) {
-        link.addEventListener('click', function () {
-            if (window.innerWidth <= 1200) {
-                navigation.classList.remove('active');
-            }
-        });
-    });
-
-    window.addEventListener('resize', function () {
-        if (window.innerWidth > 1200) {
-            navigation.classList.add('active');
-        }
-    });
-});
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
