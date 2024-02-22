73ae9f5f99a96bfd8ed19cb5dfe469844d3362d6
Raiden <adrien.graindorge@my-digital-school.org>
2024-02-22 12:42:29 +0100

    réglage du bouton de navigation

--- public/js/script.js
+++ public/js/script.js
@@ -5,28 +5,19 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(response => response.text())
         .then(html => {
             document.getElementById('header-container').innerHTML = html;
-
             // Initialiser le bouton à l'intérieur de la promesse
             let navigation = document.querySelector('.navigation');
             let toggleButton = document.querySelector('.toggle');
-            let hasClicked = false;
-
             if (navigation && toggleButton) {
                 toggleButton.addEventListener('click', function () {
-                    if (!hasClicked) {
-                        // Premier clic, changer le statut hasClicked
-                        hasClicked = true;
-                    } else {
-                        // Toggle l'état actif
-                        toggleButton.classList.toggle('active');
-                        navigation.classList.toggle('active');
-                    }
+                    toggleButton.classList.toggle('active');
+                    navigation.classList.toggle('active');
                 });
             }
 
+
             // Vérifiez la largeur de l'écran pour changer le texte du titre
             var divElement = document.querySelector('.btn');
-
             function checkScreenWidth() {
                 var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
                 var nouveauTexte = screenWidth < 425 ? 'ASMA' : 'AS Monts D\'Andaine';
